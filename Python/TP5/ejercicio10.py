5ca90a679105d6cfde5c4f4b45ff432d2f337841
Miguel <myanguez33@gmail.com>
2025-07-21 10:32:01 -0300

    Comencé el ejercicio 10

---
+++ Python/TP5/ejercicio10.py
@@ -0,0 +1,41 @@
+condicion = 0
+clientes = {
+    12345678 : {
+        'Nombre' : 'Juan Pérez',
+        'Dirección' : 'Av. Siempre Viva 742',
+        'Télefono' : '1112345678',
+        'Correo' : 'juan.perez@email.com',
+        'Preferente' : True
+    },
+    23456789: {
+        'Nombre' : 'María Gómez',
+        'Dirección' : 'Calle Falsa 123',
+        'Télefono' : '1123456789',
+        'Correo' : 'maria.gomez@email.com',
+        'Preferente' : False
+    },
+    34567890: {
+        'Nombre' : 'Carlos López',
+        'Dirección' : 'Ruta 9 Km 42',
+        'Télefono' : '1134567890',
+        'Correo' : 'carlos.lopez@email.com',
+        'Preferente' : False
+    }
+}
+print('1. Añadir cliente')
+print('2. Eliminar cliente')
+print('3. Mostrar cliente')
+print('4. Listar todos los clientes')
+print('5. Listar clientes preferentes')
+print('6. Terminar')
+
+while True:
+
+    if condicion == 6:
+        break
+    elif condicion == 1:
+        
+    elif condicion == 2:
+    elif condicion == 3:
+    elif condicion == 4:
+    elif condicion == 5:
