1df14f28939f43fe903084b89a225c80b538fe3d
Miguel <myanguez33@gmail.com>
2025-07-21 11:04:28 -0300

    Avanc}é con el ejercicio 10

--- Python/TP5/ejercicio10.py
+++ Python/TP5/ejercicio10.py
@@ -1,4 +1,4 @@
-condicion = 0
+diccionario_temporal = {}
 clientes = {
     12345678 : {
         'Nombre' : 'Juan Pérez',
@@ -28,14 +28,42 @@ print('3. Mostrar cliente')
 print('4. Listar todos los clientes')
 print('5. Listar clientes preferentes')
 print('6. Terminar')
+condicion = int(input('Elija una opción: '))
 
 while True:
 
+    #Terminar
     if condicion == 6:
         break
+    #Añadir cliente
     elif condicion == 1:
-        
+        DNI = int('Ingrese el DNI del cliente: ')
+        diccionario_temporal['Nombre'] = input('Ingrese el nombre completo del cliente: ')
+        diccionario_temporal['Direccion'] = input('Ingrese la direccón del cliente: ')
+        diccionario_temporal['Telefono'] = input('Ingrese el número de télefono del cliente: ')
+        diccionario_temporal['Mail'] = input('Ingrese el mail del cliente')
+        preferencia = input('¿El cliente es preferente?: ')
+        preferencia.upper()
+        if preferencia == 'SI':
+            diccionario_temporal['Preferente'] = True
+        elif preferencia == 'NO':
+            diccionario_temporal['Preferente'] = False
+        clientes[DNI] = diccionario_temporal
+        diccionario_temporal.clear()
+    #Eliminar cliente
     elif condicion == 2:
+        del clientes[input('Ingrese el DNI del cliente que desea eliminar: ')]
+    #Mostrar cliente
     elif condicion == 3:
+    #Listar clientes
     elif condicion == 4:
+    #Listar clientes preferentes
     elif condicion == 5:
+        print('')
+    print('1. Añadir cliente')
+    print('2. Eliminar cliente')
+    print('3. Mostrar cliente')
+    print('4. Listar todos los clientes')
+    print('5. Listar clientes preferentes')
+    print('6. Terminar')
+    condicion = int(input('Elija una opción: '))
